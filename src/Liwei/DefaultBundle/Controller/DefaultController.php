1b9e898779ea4cbbdfb77e6acb4aea5c22a9171d
Elaine <Elaine@Elaine-PC>
2014-07-06 16:34:27 +0200

    // Git test

--- src/Liwei/DefaultBundle/Controller/DefaultController.php
+++ src/Liwei/DefaultBundle/Controller/DefaultController.php
@@ -9,7 +9,7 @@ class DefaultController extends Controller
      
     public function indexAction()
     {
-        echo "gqdkfjqdklfjqskdfjqsf";
+        
         $name = "Blog";
         return $this->render('LiweiDefaultBundle:Default:index.html.twig', array('name' => $name));
     }
